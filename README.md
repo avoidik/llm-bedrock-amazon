ed0d8622fdf5fbaec21d49289a46481c36471267
Viacheslav Vasilyev <avoidik@gmail.com>
2024-07-12 15:49:33 +0100

    Make sure it is actually working

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # llm-bedrock-amazon
 
+Install from the GitHub repository:
+
 ```terminal
 $ git clone https://github.com/avoidik/llm-bedrock-amazon
 $ llm install -e llm-bedrock-amazon
+```
+
+Or, from the local directory:
+
+```terminal
+$ llm install -e .
 ```
--- llm_bedrock_titan.py
+++ llm_bedrock_titan.py
@@ -1,60 +1,78 @@
-from typing import Optional, List
+"""LLM plugin to invoke AWS Amazon Titan model in Bedrock."""
 
+from typing import Iterator, Optional
+
+import json
 import boto3
 import llm
-import json
 from pydantic import Field, field_validator
 
-# Much of this code is derived from https://github.com/tomviner/llm-claude
-# Much of this code is derived from https://github.com/sblakey/llm-bedrock-anthropic
+# Much of this code is derived from:
+# - https://github.com/tomviner/llm-claude
+# - https://github.com/sblakey/llm-bedrock-anthropic
+
 
 @llm.hookimpl
 def register_models(register):
+    """Register model in LLM."""
     register(
         BedrockTitan("amazon.titan-text-express-v1"),
         aliases=("bedrock-titan-express", "bte"),
     )
 
+
 class BedrockTitan(llm.Model):
+    """AWS Amazon Titan model in Bedrock."""
     can_stream: bool = True
     model_id: str
 
     class Options(llm.Options):
+        """Configuration of AWS Amazon Titan model."""
         max_token_count: Optional[int] = Field(
-            description="The maximum number of tokens to generate in the response.",
+            description="The maximum number of tokens to generate in the "
+                        "response.",
             default=4096,
         )
 
         temperature: Optional[float] = Field(
-            description="Use a lower value to decrease randomness in responses.",
+            description="Use a lower value to decrease randomness in the "
+                        "response.",
             default=0.7,
         )
 
         diversity: Optional[float] = Field(
-            description="Use a lower value to ignore less probable options and decrease the diversity of responses.",
+            description="Use a lower value to ignore less probable options "
+                        "and decrease the diversity of responses.",
             default=0.9,
         )
 
         @field_validator("max_token_count")
-        def validate_length(cls, max_token_count):
+        @classmethod
+        def validate_max_token_count_length(cls, max_token_count):
+            """Check max_token_count length."""
             if not (0 < max_token_count <= 8192):
                 raise ValueError("max_token_count must be in range 1-8192")
             return max_token_count
 
         @field_validator("temperature")
-        def validate_length(cls, temperature):
+        @classmethod
+        def validate_temperature_length(cls, temperature):
+            """Check temperature length."""
             if not (0.0 < temperature <= 1.0):
                 raise ValueError("temperature must be in range 0.0-1.0")
             return temperature
 
         @field_validator("diversity")
-        def validate_length(cls, diversity):
+        @classmethod
+        def validate_diversity_length(cls, diversity):
+            """Check diversity length."""
             if not (0.0 < diversity <= 1.0):
                 raise ValueError("diversity must be in range 0.0-1.0")
             return diversity
 
     def __init__(self, model_id):
         self.model_id = model_id
+        self.bedrock = boto3.client(service_name="bedrock-runtime")
 
     def execute(
         self,
@@ -78,21 +96,20 @@ class BedrockTitan(llm.Model):
 
         chunks = []
         if stream:
-            for chunk in generate_text_streaming(json.dumps(body)):
+            for chunk in self.generate_text_streaming(json.dumps(body)):
                 chunks.append(chunk)
                 yield chunk
         else:
-            response_body = generate_text(json.dumps(body))
+            response_body = self.generate_text(json.dumps(body))
             for chunk in response_body:
                 chunks.append(chunk)
                 yield chunk
 
-        response.response_json = chunks
+        response.response_json = {"messages": chunks}
 
     def generate_text_streaming(self, body):
-        bedrock = boto3.client(service_name="bedrock-runtime")
-
-        response = bedrock.invoke_model_with_response_stream(
+        """Invoke AWS Titan Model with the streaming support."""
+        response = self.bedrock.invoke_model_with_response_stream(
             accept="application/json",
             contentType="application/json",
             body=body,
@@ -106,9 +123,8 @@ class BedrockTitan(llm.Model):
                 yield chunk["outputText"]
 
     def generate_text(self, body):
-        bedrock = boto3.client(service_name="bedrock-runtime")
-
-        response = bedrock.invoke_model(
+        """Invoke AWS Titan Model without the streaming support."""
+        response = self. bedrock.invoke_model(
             accept="application/json",
             contentType="application/json",
             body=body,
@@ -116,4 +132,5 @@ class BedrockTitan(llm.Model):
         )
 
         model_response = json.loads(response["body"].read())
-        return model_response["results"][0]["outputText"]
+        for result in model_response["results"]:
+            yield result["outputText"]
